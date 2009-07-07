2883b0ae9a4bcaa443541285304c891d7d8fd5fb
Lang Martin <lang@mac-the-knife.local>
2009-07-06 21:40:55 -0400

    index.org, sitemap.org

--- org-config.el
+++ org-config.el
@@ -1,14 +1,24 @@
 (require 'org-publish)
 
-(setq
- the-blag-publishing
- `("the-blag"
-   :base-directory "blog"
-   :publishing-directory "~/Public/blog"
-   :publishing-function 'org-publish-org-to-html
-   :auto-index t
-   :index-filename "sitemap.html"
-   ))
+(let ((top "~/Public/public/")
+      (code-dir (lambda (dir)
+                  `(,dir
+                    :base-directory ,(concat "public/" dir)
+                    :base-extension "js\\|el\\|scm"
+                    :publishing-directory ,(concat top dir)
+                    :publishing-function org-publish-attachment))))
+  (setq
+   org-publish-project-alist
+   `(("orgfiles"
+      :base-directory "public"
+      :publishing-directory ,top
+      :publishing-function org-publish-org-to-html
+      :auto-index t
+      :index-filename "sitemap.org"
+      )
 
-(add-to-list 'org-publish-project-alist
-             the-blag-publishing)
+     ,(funcall code-dir "continuations")
+     ,(funcall code-dir "ht")
+
+     ("the-blag" :components ("orgfiles" "ht" "continuations"))
+     )))
