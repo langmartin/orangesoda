d5d4a52a86525ffd19d6bf9343d3df293dd93714
Lang Martin <lang@mac-the-knife.local>
2009-10-11 14:08:13 -0400

    add emacs-lisp, publish in sites

--- org-config.el
+++ org-config.el
@@ -1,17 +1,17 @@
 (require 'org-publish)
 
-(let* ((top "~/Public/public/")
+(let* ((top "~/Sites/orangesoda/")
        (components '("orgfiles"))
-       
+
        (code-dir
 	(lambda (dir)
-	  (setq components (cons dir components))	  
+	  (setq components (cons dir components))
 	  `(,dir
 	    :base-directory ,(concat "public/" dir)
 	    :base-extension "js\\|el\\|scm"
 	    :publishing-directory ,(concat top dir)
 	    :publishing-function org-publish-attachment)))
-       
+
        (static-media
 	(lambda (dir)
 	  (setq components (cons dir components))
@@ -20,14 +20,14 @@
 	    :base-extension "js\\|css\\|png\\|jpg\\|gif"
 	    :publishing-directory ,(concat top dir)
 	    :publishing-function org-publish-attachment))))
-  
+
   (setq
    org-publish-project-alist
-   `(("orgfiles"
+   `(("orangesoda"
       :base-directory "public"
       :publishing-directory ,top
       :publishing-function org-publish-org-to-html
-      
+
       :auto-index t
       :index-filename "sitemap.org"
 
@@ -36,7 +36,7 @@
       :todo-keywords nil
       :priority nil
       :auto-postamble nil
-      
+
       :style-include-default nil
       :style "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/site.css\" />")
 
@@ -46,9 +46,9 @@
      ,(funcall code-dir "ht")
      ,(funcall code-dir "scheme")
      ,(funcall code-dir "javascript")
-;     ,(funcall code-dir "continuations")     
+     ,(funcall code-dir "emacs-lisp")
 
-     ("the-blag" :components ,components)))  
+     ("the-blag" :components ,components)))
   )
 
 
