1e8b32013452692b66e6f1de0a6a5096ef045319
Lang Martin <lang@host-68-169-154-105.wisolt2.epbfi.com>
2009-11-09 20:28:54 -0500

    kind of simplify it, I was still using charclass to define characters allowed in symbols.

--- public/jsmacs/reader.js
+++ public/jsmacs/reader.js
@@ -1,57 +1,45 @@
 var jsmacs = {elisp: {}};
 
 (function () {
-   var reader = jsmacs.elisp.reader = function (text) {
-     var ii = 0;
-     function peek () {
-       if (ii > text.length) return false;
-       return text.charAt(ii);
-     }
-     function read () {
-       var ch = peek();
-       ii = ii + 1;
+   var stringport = function (text) {
+     this.text = text;
+     this.line = 1;
+     this.column = 1;
+     this.index = 0;
+   };
+   stringport.prototype = {
+     peek: function () {
+       if (this.index > this.text.length) return false;
+       return this.text.charAt(this.index);
+     },
+     read: function () {
+       var ch = this.peek();
+       this.index = this.index + 1;
+       this.column = this.column + 1;
+       if (ch == "\n") {
+         this.line = this.line + 1;
+         this.column = 1;
+       }
        return ch;
      }
-     return reader.parser(read, peek);
    };
 
-   function whitespacep (ch) {
-     return (ch.search(/^[ \r\n\t\v]$/) >= 0);
-   }
-   function numericp (ch) {
-     return (ch.search(/^\d$/) >= 0);
-   }
-
-   function charclass (ch) {
-     var cls;
-     switch (ch) {
-     case "(": cls = "lbegin"; break;
-     case ")": cls = "lend"; break;
-     case "'": cls = "quote"; break;
-     case "`": cls = "quasi"; break;
-     case "[": cls = "vbegin"; break;
-     case "]": cls = "vend"; break;
-     case "?": cls = "char"; break;
-     case "\"": cls = "string"; break;
-     }
-     if (cls) return cls;
-     else if (whitespacep(ch)) cls = "space";
-     else if (numericp(ch)) cls = "number";
-     else cls = "symbol";
-     return cls;
-   }
+   var reader = jsmacs.elisp.reader = function (text) {
+     var port = new stringport(text);
+     return reader.parser(port);
+   };
 
    function eReader (text) {
      throw new TypeError("Reader parse error: " + text);
    }
 
-   reader.parser = function (read, peek) {
-     var state = "zero";
+   reader.parser = function (port) {
+     var peek = function () { return port.peek(); };
+     var read = function () { return port.read(); };
 
-     //// Inclusive states should start accumulating with the character
-     //// they're passed, and should peek, reading only when they
-     //// accumulate. Exclusive states should just read.
-
+     /// Inclusive states should start accumulating with the character
+     /// they're passed, and should peek, reading only when they
+     /// accumulate. Exclusive states should just read.
      function inclusive (head, legal) {
        var sym = [head];
        var ch;
@@ -62,6 +50,28 @@ var jsmacs = {elisp: {}};
          sym.push(read());
        }
        return sym.join('');
+     }
+
+     // This is only used by inclusive, I don't feel like creating an
+     // inclusive symbol definition. I was using it everywhere, now I
+     // have code duplication instead.
+     function charclass (ch) {
+       var cls;
+       switch (ch) {
+       case "(": cls = "lbegin"; break;
+       case ")": cls = "lend"; break;
+       case "'": cls = "quote"; break;
+       case "`": cls = "quasi"; break;
+       case "[": cls = "vbegin"; break;
+       case "]": cls = "vend"; break;
+       case "?": cls = "char"; break;
+       case "\"": cls = "string"; break;
+       }
+       if (cls) return cls;
+       else if (whitespacep(ch)) cls = "space";
+       else if (numericp(ch)) cls = "number";
+       else cls = "symbol";
+       return cls;
      }
 
      function symbol (head) {
@@ -96,37 +106,39 @@ var jsmacs = {elisp: {}};
        return new String(str.join(''));
      }
 
-     function token (ch, cls) {
-       if (cls == "lbegin") return list();
-       else if (cls == "symbol") return symbol(ch);
-       else if (cls == "string") return string(ch);
-       else if (cls == "number") return number(ch);
-       else if (cls == "quote") return ["quote", list()];
-       else if (cls == "quasi") return ["quasiquote", list()];
-       else throw eReader(cls);
+     function token (ch) {
+       switch (ch) {
+       case "(": return list();
+       case "'": return ["quote", list()];
+       case "`": return ["quasiquote", list()];
+       case "[": return vector();
+       case "?": return character();
+       case "\"": return string();
+       }
+       if (numericp(ch)) return number(ch);
+       if (whitespacep(ch)) return token(read());
+       else return symbol(ch);
+       throw eReader();
      }
 
      function vector (head) {
        var vec = [];
-       var ch, cls;
+       var ch;
        while (true) {
          ch = read();
          if (ch === false) break;
-         cls = charclass(ch);
-         if (cls == "vend") break;
-         vec.push(token(ch, cls));
+         if (ch == "]") break;
+         vec.push(token(ch));
        }
        return vec;
      }
 
      function list () {
-       var ch, cls;
-       ch = read();
+       var ch = read();
        if (ch === false) return null;
-       cls = charclass(ch);
-       if (cls == "lend") return null;
-       if (cls == "space") return list();
-       return [token(ch, cls), list()];
+       if (ch == ")") return null;
+       if (whitespacep(ch)) return list();
+       return [token(ch), list()];
      }
 
      return list();
