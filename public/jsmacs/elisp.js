c097291b191a43fdbca9b0c430b2b989e07753ec
Lang Martin <lang@mac-the-knife.local>
2009-11-22 17:40:53 -0500

    new stubbed evaluator

--- public/jsmacs/elisp.js
+++ public/jsmacs/elisp.js
@@ -25,4 +25,41 @@
      if (isprim(word)) 1;
      if (iscall(word)) 1;
    };
+
+
+
+   function environment (env) {
+     var frame = {};
+     frame.__proto__ = env;
+     return frame;
+   }
+
+   function atomp (obj) {
+     return typeof obj != "object";
+   }
+
+   function lambdap (obj) {
+     return expressionp(obj) && (car(obj) == "lambda");
+   }
+
+   function analyse (expr) {
+     
+   }
+
+   function evaluate (env, expr) {
+     if (atomp(expr)) return expr;
+     if (lambdap(expr)) return analyse(expr);
+     if (formp(expr)) {
+       return evaluate(env, expand(env, expr));
+     }
+     else throw new TypeError("Invalid expression.");
+   }
+
+   function main (proc) {
+     var env = environment({});
+     while (proc) {
+       proc = apply(proc, arguments);
+     }
+   }
+
 })();
