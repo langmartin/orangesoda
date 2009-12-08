b9be7a482a65f6aba88aa692d5df5452d3d6e8ee
Lang Martin <lmartin@tva.gov>
2009-12-08 10:41:28 -0500

    org-config.el rewrite #3, cause I'm that ridiculous. it's tolerably neat and has all the data up top

--- org-config.el
+++ org-config.el
@@ -13,69 +13,73 @@
 ;;;    This layer of indirection takes care of the username variation, and
 ;;;    generally simplifies things.
 
+;;; I'm using fold from srfi-1, which is in this repository. Add
+;;; public/emacs-lisp to your load-list in .emacs.
+(require 'srfi-1)
 
-(if (not (boundp 'orange-soda-publish-directory))
-    (setq orange-soda-publish-directory "/orangesoda:/home/public/"))
+(if (not (boundp 'org-orangesoda-publish-directory))
+    (setq org-orangesoda-publish-directory "/orangesoda:/home/public/"))
 
-(defun org-orangesoda-static (dir)
-  (let ((src (concat default-directory "public/"))
-        (name (concat "orangesoda-static-" dir)))
-    `(,name
-      :base-directory ,(concat src dir)
-      :base-extension "js\\|el\\|scm\\|html\\|css\\|png\\|jpg\\|gif"
-      :publishing-directory ,(concat orange-soda-publish-directory dir)
-      :publishing-function org-publish-attachment)))
+(setq org-orangesoda-module-list
+      '("squash" "jsmacs"))
 
-(defun org-orangesoda-submodule (dir)
-  (let* ((src (concat default-directory "public/"))
-         (pub orange-soda-publish-directory)
-         (base (if (string= "" dir) src (concat src dir)))
-         (publish (if (string= "" dir) pub (concat pub dir)))
-         (name (if (string= "" dir) "orangesoda.net"
-                 (concat "orangesoda-" dir))))
-    (let ((module
-           `(,name
-             :base-directory ,base
-             :publishing-directory ,publish
-             :publishing-function org-publish-org-to-html
-             :link-home "http://orangesoda.net/"
-             :table-of-contents nil
-             :section-numbers nil
-             :todo-keywords nil
-             :priority nil
-             :auto-postamble nil
-             :style-include-default nil
-             :style "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/site.css\" />")))
-      (if (string= "" dir)
-          (setq module
-                (append module
-                        '(:auto-index t :index-filename "sitemap.org"))))
-      module)))
+(setq org-orangesoda-static-list
+      '("css" "js" "scheme" "javascript" "emacs-lisp"))
 
-(defun org-orangesoda-build ()
-  (let ((project '()))
-    (setq project
-          (append
-           (mapcar 'org-orangesoda-submodule
-                   '("" "squash" "jsmacs"))
-           (mapcar 'org-orangesoda-static
-                   '("css"
-                     "js"
-                     "scheme"
-                     "javascript"
-                     "emacs-lisp"
-                     "squash" "squash/js"
-                     "jsmacs"
-                     ))))
-    (setq project
-          (cons (cons "orangesoda"
-                      (list :components
-                            (mapcar 'car project)))
-                project))
-    project))
+(setq org-orangesoda-module-options
+      (list
+       :publishing-function 'org-publish-org-to-html
+       :link-home "http://orangesoda.net/"
+       :table-of-contents nil
+       :section-numbers nil
+       :todo-keywords nil
+       :priority nil
+       :auto-postamble nil
+       :style-include-default nil
+       :style
+       "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/site.css\"/>"))
 
-(defun org-orangesoda-clobber ()
-  (interactive)
-  (setq org-publish-project-alist (org-orangesoda-build)))
+(setq org-orangesoda-static-options
+      (list
+       :publishing-function 'org-publish-attachment
+       :base-extension "js\\|el\\|scm\\|html\\|css\\|png\\|jpg\\|gif"))
 
-(org-orangesoda-clobber)
+(defun org-orangesoda-make-alist ()
+  (let* ((src-root (concat default-directory "public/"))
+         (module org-orangesoda-module-options)
+         (static org-orangesoda-static-options)
+         (cons (lambda (name src pub options tail)
+                 (cons (append (list name
+                                     :base-directory src
+                                     :publishing-directory pub)
+                               options)
+                       tail)))
+         (path (lambda (path pfix opts acc)
+                 (funcall cons
+                          (concat "orangesoda-" pfix "-" x)
+                          (concat src-root x)
+                          (concat org-orangesoda-publish-directory x)
+                          opts
+                          acc))))
+    (fold (lambda (x acc)
+            (funcall path x "static" static acc))
+          (fold (lambda (x acc)
+                  (funcall path x "static" static
+                           (funcall path x "module" module acc)))
+                (funcall cons "orangesoda-static"
+                         src-root
+                         org-orangesoda-publish-directory
+                         static
+                         (funcall cons "orangesoda-module"
+                                  src-root
+                                  org-orangesoda-publish-directory
+                                  module
+                                  nil))
+                org-orangesoda-module-list)
+          org-orangesoda-static-list)))
+
+(setq org-publish-project-alist (org-orangesoda-make-alist))
+(setq org-publish-project-alist
+      (cons (list "orangesoda"
+                  :components (mapcar 'car org-publish-project-alist))
+            org-publish-project-alist))
